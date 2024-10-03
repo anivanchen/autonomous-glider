d9e612091fdaa4ac4865f4f4833b4a3ca3629c7f
Jack Cammarata <jtc6239@rit.edu>
2024-10-03 02:02:08 -0400

    add some details about usage

--- src/glider.cpp
+++ src/glider.cpp
@@ -4,6 +4,25 @@
 
 int main()
 {
+    // the way we should go about doing this is
+
+    // wait for an output from either imu or gps
+
+    // if imu measurement, run predict using dt since last run
+    // save timestamp
+
+    // if gps measurement, run predict using dt since last run
+    // run update using dt since last run
+    // save timestamp
+
+    // this loop continues forever
+
+    // sr_ukf.get_xhat() returns an Eigen::Vector containing
+    // x y z vx vy vz yaw pitch roll as floats
+
+    // if possible maybe run the kalman filter on a separate thread from the glider control logic?
+    // not sure whether this is possible on these microcontrollers
+
     Eigen::Vector<float, 9> initial {0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5};
     Eigen::Vector<float, 9> initial_stddevs {0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1};
     Eigen::Vector<float, 9> process_stddevs {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
