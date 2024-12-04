bd5e39ba0271ca5a55d13c2ff7649155eedb6313
Ivan Chen <ivanchen07@gmail.com>
2024-12-03 22:55:09 -0500

    Start implementation of ICM20948 driver

---
+++ src/icm20948.cpp
@@ -0,0 +1,154 @@
+#include "icm20948.h"
+
+icm20948_return_code_t icm20948_init() {
+  sleep_ms(1000); // Short delay for boot up
+  uint8_t reg = B0_WHO_AM_I;
+  uint8_t chipID[1];
+  i2c_write_blocking(I2C_PORT, I2C_ADDR, &reg, 1, true);
+  i2c_read_blocking(I2C_PORT, I2C_ADDR, chipID, 1, false);
+
+  if (chipID[0] != ICM20948_WHO_AM_I_DEFAULT) {
+    printf("Chip ID DOES NOT MATCH 0xEA: 0x%02X\n", chipID[0]);
+    return ICM20948_RET_GEN_FAIL;
+  }
+
+  printf("ICM20948 detected\n");
+  printf("Chip ID MATCHES 0xEA: 0x%02X\n", chipID[0]);
+
+  // Set clock source
+
+  uint8_t data[2] = {B0_PWR_MGMT_1, 0x01};
+  i2c_write_blocking(I2C_PORT, I2C_ADDR, data, 2, true);
+
+  // Ensure accelerometer and gyroscope are enabled
+
+  data[0] = B0_PWR_MGMT_2;
+  data[1] = 0x00;
+  i2c_write_blocking(I2C_PORT, I2C_ADDR, data, 2, true);
+
+  // Change to user bank 2
+  data[0] = ICM20948_REG_BANK_SEL;
+  data[1] = ICM20948_USER_BANK_2;
+  i2c_write_blocking(I2C_PORT, I2C_ADDR, data, 2, true);
+
+  // Configure gyro
+  data[0] = B2_GYRO_CONFIG_1;
+  data[1] = 0x29;
+  i2c_write_blocking(I2C_PORT, I2C_ADDR, data, 2, true);
+
+  // Set gyro rate
+  data[0] = B2_GYRO_SMPLRT_DIV;
+  data[1] = 0x00;
+  i2c_write_blocking(I2C_PORT, I2C_ADDR, data, 2, true);
+
+  // Configure accel
+  data[0] = B2_ACCEL_CONFIG;
+  data[1] = 0x29;
+
+  // Set accel rate
+  data[0] = B2_ACCEL_SMPLRT_DIV_1;
+  data[1] = 0x00;
+  i2c_write_blocking(I2C_PORT, I2C_ADDR, data, 2, true);
+
+  data[0] = B2_ACCEL_SMPLRT_DIV_2;
+  data[1] = 0x00;
+  i2c_write_blocking(I2C_PORT, I2C_ADDR, data, 2, true);
+
+  // Change to user bank 0
+  data[0] = ICM20948_REG_BANK_SEL;
+  data[1] = ICM20948_USER_BANK_0;
+  i2c_write_blocking(I2C_PORT, I2C_ADDR, data, 2, true);
+
+  return ICM20948_RET_OK;
+}
+
+icm20948_return_code_t icm20948_applySettings(icm20948_settings_t *newSettings) {
+
+}
+
+icm20948_return_code_t icm20948_getGyroData(icm20948_gyro_t *data) {
+
+  uint8_t reg = B0_INT_STATUS_1;
+  uint8_t ready[1];
+
+  i2c_write_blocking(I2C_PORT, I2C_ADDR, &reg, 1, true);
+  i2c_read_blocking(I2C_PORT, I2C_ADDR, ready, 1, false);
+
+  // reg = B0_WHO_AM_I;
+  // uint8_t chipID[1];
+  // i2c_write_blocking(I2C_PORT, I2C_ADDR, &reg, 1, true);
+  // i2c_read_blocking(I2C_PORT, I2C_ADDR, chipID, 1, false);
+
+  // printf("ID: 0x%02X\n", chipID[0]);
+
+  if (ready[0] == 1) {
+    printf("Gyro data ready\n");
+
+    reg = B0_GYRO_XOUT_H;
+    uint8_t gyroData[6];
+    i2c_write_blocking(I2C_PORT, I2C_ADDR, &reg, 1, true);
+    i2c_read_blocking(I2C_PORT, I2C_ADDR, gyroData, 6, false);
+
+    printf("X: %d, Y: %d, Z: %d\n", (int16_t)(gyroData[1]), (int16_t)(gyroData[3]), (int16_t)(gyroData[5]));
+  }
+
+  if (ready[0] == 0) {
+    printf("Gyro data not ready\n");
+  }
+  
+  return ICM20948_RET_OK;
+}
+
+icm20948_return_code_t icm20948_getAccelData(icm20948_accel_t *data) {
+
+  uint8_t reg = B0_INT_STATUS_1;
+  uint8_t ready[1];
+
+  i2c_write_blocking(I2C_PORT, I2C_ADDR, &reg, 1, true);
+  i2c_read_blocking(I2C_PORT, I2C_ADDR, ready, 1, false);
+
+  // reg = B0_WHO_AM_I;
+  // uint8_t chipID[1];
+  // i2c_write_blocking(I2C_PORT, I2C_ADDR, &reg, 1, true);
+  // i2c_read_blocking(I2C_PORT, I2C_ADDR, chipID, 1, false);
+
+  // printf("ID: 0x%02X\n", chipID[0]);
+
+  if (ready[0] == 1) {
+    printf("Accel data ready\n");
+
+    reg = B0_ACCEL_XOUT_H;
+    uint8_t accelData[6];
+    i2c_write_blocking(I2C_PORT, I2C_ADDR, &reg, 1, true);
+    i2c_read_blocking(I2C_PORT, I2C_ADDR, accelData, 6, false);
+
+    printf("X: %d, Y: %d, Z: %d\n", (int16_t)(accelData[1]), (int16_t)(accelData[3]), (int16_t)(accelData[5]));
+  }
+
+  if (ready[0] == 0) {
+    printf("Accel data not ready\n");
+  }
+
+}
+
+icm20948_return_code_t icm20948_getMagData(icm20948_mag_t *data) {
+
+}
+
+icm20948_return_code_t icm20948_getTempData() {
+  // reg = B0_WHO_AM_I;
+  // uint8_t chipID[1];
+  // i2c_write_blocking(I2C_PORT, I2C_ADDR, &reg, 1, true);
+  // i2c_read_blocking(I2C_PORT, I2C_ADDR, chipID, 1, false);
+
+  // printf("ID: 0x%02X\n", chipID[0]);
+
+  uint8_t reg = B0_TEMP_OUT_H;
+  uint8_t tempData[2];
+  i2c_write_blocking(I2C_PORT, I2C_ADDR, &reg, 1, true);
+  i2c_read_blocking(I2C_PORT, I2C_ADDR, tempData, 6, false);
+
+  printf("Temp: %d\n", tempData[1]);
+
+  return ICM20948_RET_OK;
+}
