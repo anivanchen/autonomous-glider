3124a7e5b98d04df5db8f09f31c471212ded1b59
Ivan Chen <ivanchen07@gmail.com>
2025-01-21 11:38:04 -0500

    Add testing code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "icm20948.h"
+#include "pwmservo.h"
 
 int main() {
 
@@ -7,24 +8,47 @@ int main() {
   stdio_init_all();
 
   // Initialize I2C port at 100 kHz
-  i2c_init(I2C_PORT, 400 * 1000);
-  gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
-  gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
-  gpio_pull_up(I2C_SDA_PIN);
-  gpio_pull_up(I2C_SCL_PIN);
+  // i2c_init(I2C_PORT, 400 * 1000);
+  // gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
+  // gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
+  // gpio_pull_up(I2C_SDA_PIN);
+  // gpio_pull_up(I2C_SCL_PIN);
 
-  icm20948_init();
-  enable_dmp();
+  // icm20948_init();
+  // enable_dmp();
+
+  Servo leftAileronServo = Servo(29);
+  // Servo rightAileronServo = Servo(3);
+  // Servo elevatorServo = Servo(4);
 
   // Primary loop
 
   while (1) {
 
-    icm20948_gyro_t gyro_data;
-    icm20948_accel_t accel_data;
+    // icm20948_gyro_t gyro_data;
+    // icm20948_accel_t accel_data;
 
-    icm20948_getData(&gyro_data, &accel_data);
-    read_dmp();
+    // icm20948_getData(&gyro_data, &accel_data);
+    // read_dmp();
+
+    // leftAileronServo.setPosition(900); // MIN
+    // sleep_ms(1000);
+    // leftAileronServo.setPosition(2100); // MAX
+    // sleep_ms(1000);
+    // leftAileronServo.setPosition(1500); // CENTER
+    // sleep_ms(1000);
+
+    for (int i = 900; i < 2100; i += 50) {
+      leftAileronServo.setPosition(i);
+      sleep_ms(50);
+      if (i == 2100) {
+        i = 900;
+      }
+    }
+
+    // rightAileronServo.setPosition(50);
+    // elevatorServo.setPosition(50);
+    
     // icm20948_getTempData();
 
   }
