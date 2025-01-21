0575e80c6f8a77fbec4dc61dd715ec4abdddc3b8
Ivan Chen <ivanchen07@gmail.com>
2025-01-21 11:37:34 -0500

    Add working pwm servo class

---
+++ src/pwmservo.cpp
@@ -0,0 +1,19 @@
+#include "pwmservo.h"
+
+Servo::Servo(uint8_t servoPin) {
+  this->servoPin = servoPin;
+
+  gpio_set_function(servoPin, GPIO_FUNC_PWM);
+
+  uint slice_num = pwm_gpio_to_slice_num(servoPin);
+  pwm_config config = pwm_get_default_config();
+  pwm_config_set_clkdiv(&config, 16.0f);
+  pwm_set_wrap(slice_num, (125000000 / (16.0f * 50.0f)) - 1);
+  pwm_init(slice_num, &config, true);
+
+}
+
+void Servo::setPosition(uint16_t position) {
+  uint16_t level = (position * (125000000 / (16.0f * 50.0f)) - 1) / (1000000 / (125000000 / 16.0f / ((125000000 / (16.0f * 50.0f)) - 1) + 1));
+  pwm_set_gpio_level(servoPin, level);
+}
---
+++ src/pwmservo.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef __PWM_SERVO_H__
+#define __PWM_SERVO_H__
+
+#include <iostream>
+#include <stdint.h>
+#include <cstddef>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+
+#define SERVO_FREQ 50
+#define SERVO_RANGE 39062.0f
+
+class Servo {
+  public:
+    Servo(uint8_t servoPin);
+    void setPosition(uint16_t position);
+  private:
+    uint servoPin;
+};
+
+#endif
