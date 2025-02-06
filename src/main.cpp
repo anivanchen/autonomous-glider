15b69a7700dc43c0b13f4909b223e5ed14b1b097
Ivan Chen <ivanchen07@gmail.com>
2025-02-06 15:46:28 -0500

    Finish state entrance and exit code

--- src/main.cpp
+++ src/main.cpp
@@ -54,6 +54,7 @@ int main() {
   /*** State Machine Initialization ***/
 
   glider_state current_state = STEADY_FLIGHT;
+  glider_state previous_state = current_state;
 
   // GPS initialization
 
@@ -78,17 +79,32 @@ int main() {
 
   // Time tracker for LEDS
 
-  float current_time = time_us_64() / 1000000;
+  float led_blinker_time = time_us_64() / 1000000;
+  float state_transition_time = time_us_64() / 1000000;
+
+  // Transition Flags
+
+  bool isSameState = false;
+  bool steady_flight_transition = false;
 
   // Primary loop
 
   while (1) {
 
     pose current_pose = getPose();
+    float current_time = time_us_64() / 1000000;
+
+    isSameState = current_state == previous_state; 
+    previous_state = current_state;
 
     switch (current_state) {
       case STEADY_FLIGHT:
       {
+        if (!isSameState) {
+          state_transition_time = current_time;
+          steady_flight_transition = false;
+        }
+
         // get pitch to be around 0 +- 5 degrees
         // get roll to be around 0 +- 5 degrees
 
@@ -101,13 +117,22 @@ int main() {
         }
 
         // if steady flight for 5 seconds, switch to banking 180
-        // current_state = BANKING_180;
+
+        steady_flight_transition = withinTolerance(current_pose.pitch, 0, 5);
+
+        if (steady_flight_transition && state_transition_time - current_time >= 5) {
+          current_state = BANKING_180;
+          steady_flight_transition = false;
+        }
 
         break;
       }
 
       case BANKING_180:
       {
+        if (!isSameState) {
+          state_transition_time = current_time;
+        }
         // allow pitch to descend, 5 degreesish
         // start rolling right until yaw is 180 degrees
         // maximum roll rate is 3 degrees per second
@@ -124,9 +149,9 @@ int main() {
           rollPID.update(0, current_pose.roll);
         }
 
-        // if 180 degrees, switch to controlled descent
-        // current_state = CONTROLLED_DESCENT;
-        if (withinTolerance(current_pose.y, 0, 20)) {
+        // if has rotated 180 degrees +- 10 degrees and within 40 meters 
+        // of target y, switch to controlled descent
+        if (withinTolerance(current_pose.y, 0, 20) && withinTolerance(current_pose.yaw, 180, 10)) {
           current_state = CONTROLLED_DESCENT;
         }
 
@@ -135,9 +160,12 @@ int main() {
 
       case CONTROLLED_DESCENT:
       {
+        if (!isSameState) {
+          state_transition_time = current_time;
+        }
+
         // pitch down 10 degrees to start descent
         // roll left/right to be on course with the goal
-
         if (withinTolerance(current_pose.y, 0, 20)) {
           pitchPID.update(10, current_pose.pitch);
         } else {
@@ -149,9 +177,7 @@ int main() {
         }
 
         // once below 15 meters altitude, switch to landing
-        // current_state = LANDING;
-
-        if (current_pose.z < 15) {
+        if (current_pose.z <= 15) {
           current_state = LANDING;
         }
 
@@ -180,11 +206,9 @@ int main() {
 
     // Update LEDS
 
-    float new_time = time_us_64() / 1000000;
-
-    if (new_time - current_time > 1) {
+    if (current_time - led_blinker_time > 1) {
       blink_lights();
-      current_time = new_time;
+      led_blinker_time = current_time;
     }
 
   }
